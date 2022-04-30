bf54b3e4d6efdbc74fc0299bcbf65b70dc7760a7
artisyn <artioms1@gmail.com>
2022-04-30 17:02:09 +0300

    nav

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from '../navigation/Navigation.module.scss';
 import { Link } from 'react-router-dom';
 import { BiMenuAltRight } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
 
 function Navigation() {
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [currentPage, setCurrentPage] = useState('about');
+
+	const openMenu = () => {
+		setMenuOpen(true);
+	};
+
+	const closeMenu = () => {
+		setMenuOpen(false);
+	};
+
+	const changePage = (val) => {
+		setCurrentPage(val);
+		setMenuOpen(false);
+	};
+
 	return (
 		<nav className={classes.navigation}>
 			<div className={classes.logo}>LOGO</div>
-			<div className={classes.pages__container}>
-				<Link className={classes.link} to={'/about'}>
+			<div
+				className={`${classes.pages__container} ${
+					menuOpen ? classes.menu__open : ''
+				}`}
+			>
+				<Link
+					onClick={() => changePage('about')}
+					className={`${classes.link} ${
+						currentPage === 'about' ? classes.active : ''
+					}`}
+					to={'/about'}
+				>
 					About
 				</Link>
-				<Link className={classes.link} to={'/guide'}>
+				<Link
+					onClick={() => changePage('guide')}
+					className={`${classes.link} ${
+						currentPage === 'guide' ? classes.active : ''
+					}`}
+					to={'/guide'}
+				>
 					Test Guide
 				</Link>
-				<Link className={classes.link} to={'/test'}>
+				<Link
+					onClick={() => changePage('test')}
+					className={`${classes.link} ${
+						currentPage === 'test' ? classes.active : ''
+					}`}
+					to={'/test'}
+				>
 					Test
 				</Link>
+				{menuOpen && (
+					<AiOutlineClose
+						onClick={closeMenu}
+						className={classes.close__btn}
+					/>
+				)}
 			</div>
-			<div className={classes.burger}>
-				<BiMenuAltRight />
+			<div
+				className={`${classes.burger} ${
+					menuOpen ? classes.no__opacity : ''
+				}`}
+			>
+				<BiMenuAltRight onClick={openMenu} />
 			</div>
 		</nav>
 	);
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,24 @@ function About() {
 	return (
 		<div className={classes.main__container}>
 			<h1>About</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Mollitia ex sapiente adipisci pariatur dolorem at unde officia
+				eveniet consectetur, necessitatibus, iure saepe ipsa veniam
+				eaque id quaerat quisquam, quasi commodi!
+			</p>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Mollitia ex sapiente adipisci pariatur dolorem at unde officia
+				eveniet consectetur, necessitatibus, iure saepe ipsa veniam
+				eaque id quaerat quisquam, quasi commodi!
+			</p>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Mollitia ex sapiente adipisci pariatur dolorem at unde officia
+				eveniet consectetur, necessitatibus, iure saepe ipsa veniam
+				eaque id quaerat quisquam, quasi commodi!
+			</p>
 		</div>
 	);
 }
